2c776cd31625386dcf31772066d5d64ae985f56a
martapienkosz <70956012+martapienkosz@users.noreply.github.com>
2020-11-03 14:01:04 +0100

    Add files via upload

---
+++ November 3/lightgame.ino
@@ -0,0 +1,107 @@
+const int blueLED = 12;
+const int redLED = 10;
+const int greenLED = 8;
+const int yellowLED = 6;
+
+const byte blueButton = A1;
+const byte redButton = A2;
+const byte greenButton = A3;
+const byte yellowButton = A4;
+
+int BUTTONstate1 = 0;
+int BUTTONstate2 = 0;
+int BUTTONstate3 = 0;
+int BUTTONstate4 = 0;
+int cnt = 0;
+
+boolean b = false;
+boolean r = false;
+boolean g = false;
+boolean y = false;
+
+void setup() {
+  pinMode(6, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+}
+
+void draw() {
+  void loop();
+  void count();
+  void gamewin();
+}
+void loop() {
+  BUTTONstate1 = digitalRead(blueButton);
+  if (BUTTONstate1 == HIGH) {
+    digitalWrite(blueLED, HIGH);
+    delay(100);
+    b = true;
+  }
+  else {
+    digitalWrite(blueLED, LOW);
+  }
+  BUTTONstate2 = digitalRead(redButton);
+  if (BUTTONstate2 == HIGH)
+  {
+    digitalWrite(redLED, HIGH);
+    delay(100);
+    r = true;
+  }
+  else {
+    digitalWrite(redLED, LOW);
+  }
+  BUTTONstate3 = digitalRead(greenButton);
+  if (BUTTONstate3 == HIGH) {
+    digitalWrite(greenLED, HIGH);
+    delay(100);
+    g = true;
+  }
+  else {
+    digitalWrite(greenLED, LOW);
+  }
+  BUTTONstate4 = digitalRead(yellowButton);
+  if (BUTTONstate4 == HIGH) {
+    digitalWrite(yellowLED, HIGH);
+    delay(100);
+    y = true ;
+  }
+  else {
+    digitalWrite(yellowLED, LOW);
+  }
+
+  if (cnt == 10) {
+    cnt = 0;
+    digitalWrite(blueLED, HIGH);
+    delay(100);
+    digitalWrite(redLED, HIGH);
+    delay(100);
+    digitalWrite(greenLED, HIGH);
+    delay(100);
+    digitalWrite(yellowLED, HIGH);
+    delay(100);
+  }
+}
+
+void count() {
+  if (b == true) {
+    cnt = 0;
+    b = false;
+  }
+  if (r == true) {
+    cnt += 1;
+    r = false;
+  }
+  if (g == true) {
+    cnt += 3;
+    g = false;
+  }
+  if (y == true) {
+    cnt += 5;
+    y = false;
+  }
+}
